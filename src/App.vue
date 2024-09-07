2fd7d30e69614faef7a02002e8c15acffffa0fcc
Ehcraball <nathan95130.nl@gmail.com>
2024-09-07 11:54:12 +0200

    ajouter tâches sans persistances des données

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,23 @@
 <template>
-
+    <form action="" @submit.prevent="addTodo">
+        <fieldset role="group">
+            <input v-model="newTodo" type="text" placeholder="Tâche(s) à faire" />
+            <button>Ajouter</button>
+        </fieldset>
+    </form>
+    <div v-if="todos.length == 0">Vous n'avez pas de tâche à faire</div>
 </template>
 
 <script setup>
+import { ref } from "vue";
 
+const newTodo = ref('')
+const todos = ref([])
+const addTodo = () => {
+    todos.value.push({
+        title: newTodo.value,
+        completed: false,
+        date: Date.now(),
+    })
+}
 </script>

e1106cfb40e586f3f2c1a3caae2628199c07e5d2
Ehcraball <nathan95130.nl@gmail.com>
2024-09-07 13:10:28 +0200

    Ajout de sortedTodo pour masquer les taches complétés. + correction : les taches coché reste coché après redémarrage du navigateur

--- src/App.vue
+++ src/App.vue
@@ -8,17 +8,22 @@
     <div v-if="todos.length == 0">Vous n'avez pas de tâche à faire</div>
     <div v-else>
         <ul>
-            <li v-for="todo in todos" :key="todo.date" :class="{ completed: todo.completed }">
-                <input type="checkbox" v-model="todo.completed" />{{ todo.title }}
+            <li v-for="todo in sortedTodos()" :key="todo.date" :class="{ completed: todo.completed }">
+                <input type="checkbox" v-model="todo.completed" @change="saveTodos" />{{ todo.title }}
                 <button @click="editTodo(todo)">Modifier</button>
                 <button @click="deleteTodo(todo)">Supprimer</button>
             </li>
         </ul>
+        <input type="checkbox" v-model="hideCompleted">
+        Masquer les taches complétées
     </div>
 </template>
 
 <script setup>
 import { ref, onMounted } from "vue";
+
+const hideCompleted = ref(false)
+
 
 const newTodo = ref('')
 const todos = ref([{
@@ -76,6 +81,15 @@ const deleteTodo = (todo) => {
     todos.value = todos.value.filter(task => task != todo)
     saveTodos();
 }
+
+const sortedTodos = () => {
+    const sortedTodos = todos.value.toSorted((a, b) => a.completed > b.completed ? 1 : -1)
+    if (hideCompleted.value == true) {
+        return sortedTodos.filter(task => task.completed == false)
+    }
+    return sortedTodos
+}
+
 
 </script>
 
